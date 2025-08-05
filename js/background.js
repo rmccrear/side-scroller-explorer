741408ea0f19359b2924573a775b0c1039f00635
Edward Smith <213342565+EdSmith2032@users.noreply.github.com>
2025-08-04 19:25:20 -0500

    Added sun

--- js/background.js
+++ js/background.js
@@ -4,7 +4,8 @@ function drawBackground() {
     background(135, 206, 235);
     
     // TODO: Sun on the right side
-    
+    fill(255,255,0); //Yellow
+    ellipse(0,0,100,100);
     // Grass at the bottom (100px high)
     fill(34, 139, 34);
     rect(0, 400 - 100, 400, 100);
