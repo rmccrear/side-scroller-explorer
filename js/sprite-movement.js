0d58a1403b295dc62b4335147d90209ccd209ee0
TyQuincia-Oby <tyquinciaoby@gmail.com>
2025-08-04 19:47:45 -0500

    added sprite movement random

--- js/sprite-movement.js
+++ js/sprite-movement.js
@@ -3,10 +3,14 @@ function doSpriteMovement() {
     // Food movement - moves from right to left and repeats
     if(food.x < 0) {
         food.x = 400;
+        food.y = randomNumber(100, 250);
+        food.velocity=randomNumber(-1, -3);
     }
     
     // Enemy movement - moves from right to left and repeats
     if(enemy.x < 0) {
         enemy.x = 400;
+        enemy.y = randomNumber(100, 250);
+        enemy.velocity = randomNumber(-1, -3);
     }
 }
